398c9d0f1be7f37aae43a40ccbcf5910f1c76424
augustin cousin <augustincousin.pro@gmail.com>
2019-03-20 17:50:20 +0100

    test vertical align

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import Chart from './chart';
 import ItemList from './item_list';
+import Item from './item';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       skills: [
+        '<i className="devicon-css3-plain-wordmark colored"></i>',
+        '<i className="devicon-css3-plain-wordmark colored"></i>',
+        '<i className="devicon-css3-plain-wordmark colored"></i>',
+        '<i className="devicon-css3-plain-wordmark colored"></i>',
         '<i className="devicon-css3-plain-wordmark colored"></i>',
         '<i className="devicon-css3-plain-wordmark colored"></i>',
         '<i className="devicon-css3-plain-wordmark colored"></i>'
@@ -26,7 +31,14 @@ class App extends Component {
     return (
       <div className="render-wrapper">
         <Chart />
-        <ItemList skills={this.state.skills} selectedSkill={this.selectedSkill} />
+        <div className="item-wrapper">
+          <div className="item-list">
+            <ItemList skills={this.state.skills} selectedSkill={this.selectedSkill} />
+          </div>
+          <div className="item-selected">
+            <Item />
+          </div>
+        </div>
       </div>
     );
   }
