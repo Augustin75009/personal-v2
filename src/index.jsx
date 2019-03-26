385a8a9f6750f51bb0e041f05c9b85e4624c479b
augustin cousin <augustincousin.pro@gmail.com>
2019-03-26 21:44:52 +0100

    test js

--- src/index.jsx
+++ src/index.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
 
-// import '../assets/stylesheets/application.scss';
-// import '../node_modules/parallax-js/dist/parallax.min';
-// import '../node_modules/chart.js/dist/Chart';
-// import '../init';
-// import '../copy';
-// import '../open';
-// import '../parallaxScroll';
-// import '../shoWidget';
-// import '../scroll';
+import '../assets/stylesheets/application.scss';
+import '../node_modules/parallax-js/dist/parallax.min';
+import '../node_modules/chart.js/dist/Chart';
+import '../init';
+import '../copy';
+import '../open';
+import '../parallaxScroll';
+import '../shoWidget';
+import '../scroll';
+import '../test';
 
 const root = document.getElementById('root');
 if (root) {
