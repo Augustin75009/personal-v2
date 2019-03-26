c6adc8673c48aa399724e8174970b3417dcb318d
augustin cousin <augustincousin.pro@gmail.com>
2019-03-26 19:10:08 +0100

    test js importation

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,13 @@ import App from './components/app';
 
 import '../assets/stylesheets/application.scss';
 import '../node_modules/parallax-js/dist/parallax.min';
+import '../node_modules/chart.js/dist/Chart';
+import '../init';
+import '../copy';
+import '../open';
 import '../parallaxScroll';
+import '../shoWidget';
+import '../scroll';
 
 const root = document.getElementById('root');
 if (root) {
