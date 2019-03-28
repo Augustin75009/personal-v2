c4b12c0b3d1a7cb0df1b3d8e316a1e75c5f249f3
augustin cousin <augustincousin.pro@gmail.com>
2019-03-28 17:29:51 +0100

    js cloud

---
+++ js/chartInit.js
@@ -0,0 +1,51 @@
+var ctx2 = document.getElementById("myChart2");
+
+var myRadarChart2 = new Chart(ctx2, {
+  type: 'radar',
+  data: {
+    labels:
+      ["Ruby", "Rails", "C/C++", "Matlab", "Python"],
+    datasets: [{
+      data: [5, 4, 3, 2, 3],
+      borderWidth: 1,
+      borderColor: 'rgba(255,255,255,0.7)',
+      // borderColor: 'rgba(65, 156, 255, 0)',
+      backgroundColor: 'rgba(67, 176, 92, 0.7)'
+      // backgroundColor: 'rgba(65, 156, 255, 0.7)'
+    }]
+  },
+  options: {
+    responsive: true,
+    legend: {
+      display: false
+    },
+    scale: {
+      display: true,
+      angleLines: {
+        color: 'rgba(220, 220, 220, 0.1)'
+      },
+      xAxes: [{
+        display: true,
+        color: "#FFFFFF",
+        gridLines: {
+          display: true,
+          color: 'rgba(220, 220, 220, 0.3)'
+        }
+      }],
+      gridLines: {
+        display: true,
+        color: "rgba(220, 220, 220, 0.3)"
+      },
+      ticks: {
+        beginAtZero: true,
+        max: 5,
+        stepSize: 1,
+        display: false
+      },
+      pointLabels: {
+        fontSize: 17,
+        display: true,
+        fontColor: 'grey'
+      }
+    }},
+});
---
+++ js/shoWidget.js
@@ -0,0 +1,53 @@
+const widget = document.querySelector('#widget-container');
+const widgetMail = document.querySelector('#widget-mail');
+const widgetPhone = document.querySelector('#widget-phone');
+const widgetCv = document.querySelector('#widget-cv');
+const widgetClose = document.querySelector('#widget-close');
+const contentMail = document.querySelector('.content-mail');
+const contentPhone = document.querySelector('.content-phone');
+const contentCv = document.querySelector('.content-cv');
+const body1 = document.querySelector('.banner');
+const body2 = document.querySelector('.first-card');
+const body3 = document.querySelector('.second-card');
+const progress = document.querySelector('.container-scroll');
+var disableScroll = true;
+
+
+ widget.addEventListener('click', function() {
+    widgetMail.classList.add("show-widget");
+    widgetPhone.classList.add("show-widget");
+    widgetCv.classList.add("show-widget");
+    widgetClose.classList.add("show-widget");
+    contentMail.classList.add("show-content");
+    contentPhone.classList.add("show-content");
+    contentCv.classList.add("show-content");
+    body1.classList.add("opacity");
+    body2.classList.add("opacity");
+    document.getElementById("second-card").classList.remove("addOpacity");
+    document.getElementById("second-card").classList.remove("RemoveOpacity");
+    document.querySelector(".item-wrapper").classList.remove("addClass");
+    document.querySelector(".item-wrapper").classList.remove("removeClass");
+    body3.classList.add("opacity");
+    progress.classList.add("display");
+    widget.dataset.disableScroll = "true"
+    disableScroll =false;
+});
+
+  widgetClose.addEventListener('click', function() {
+    widgetMail.classList.remove("show-widget");
+    widgetPhone.classList.remove("show-widget");
+    widgetCv.classList.remove("show-widget");
+    widgetClose.classList.remove("show-widget");
+    contentMail.classList.remove("show-content");
+    contentPhone.classList.remove("show-content");
+    contentCv.classList.remove("show-content");
+    body1.classList.remove("opacity");
+    body2.classList.remove("opacity");
+    body3.classList.remove("opacity");
+    document.getElementById("second-card").classList.add("addOpacity");
+    document.getElementById("second-card").classList.remove("RemoveOpacity");
+    document.querySelector(".item-wrapper").classList.add("addClass");
+    document.querySelector(".item-wrapper").classList.remove("removeClass");
+    progress.classList.remove("display");
+    disableScroll = true;
+});
