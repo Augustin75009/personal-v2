641c04b619b2082b647cfac6cc08703bb6da2439
augustin cousin <augustincousin.pro@gmail.com>
2019-03-28 17:05:08 +0100

    Updates

---
+++ init.js
@@ -0,0 +1,8 @@
+// import Parallax from 'node_modules/parallax-js/dist/parallax.min';
+
+var scene = document.getElementById('scene');
+var parallax = new Parallax(scene);
+
+var sceneSkills = document.getElementById('scene-skills');
+var parallax = new Parallax(sceneSkills);
+
---
+++ setClass.js
@@ -0,0 +1,12 @@
+const skillsCard = document.querySelectorAll(".skills-card");
+let initFirst = 0;
+skillsCard[initFirst].classList.add("selected");
+
+skillsCard.forEach((skill, index) => {
+  skill.addEventListener("click", function() {
+    skillsCard[initFirst].classList.remove("selected");
+    skillsCard[index].classList.add("selected");
+    initFirst = index;
+
+  });
+});
